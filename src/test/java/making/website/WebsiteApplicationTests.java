0438bcdd92965852badbf29e6d646241d1625c3a
youngGit <ny2485@naver.com>
2022-05-31 23:35:36 +0900

    Initial commit

---
+++ src/test/java/making/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package making.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
